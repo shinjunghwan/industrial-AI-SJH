cdf496c6f6c0ebd6ebd75877bb3df3fc8a5cac00
shinjunghwan <hwani3115@naver.com>
2021-08-16 16:31:14 +0900

    Create prac_12.py

---
+++ programming/Python/prac_12.py
@@ -0,0 +1,11 @@
+def get_sum(start, end):
+  sum = 0
+  for i in range(start,end+1):
+    sum += i
+  return sum
+
+value = get_sum(1,10)
+print(value)
+
+value = get_sum(10, get_sum(1,10))
+print(value)
